243a14bf8b6ed485fae442958add8e62d8efa8f5
KEUNWOO KANG <52201658+moaikang@users.noreply.github.com>
2023-06-19 01:23:38 +0900

    feat: next.config.js 속성 변경 = 빌드 타임에 타입체크, 린트 체크 하지 않게 (#6)
    Co-authored-by: keunwoo-kang <keunwoo.kang@toss.im>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
