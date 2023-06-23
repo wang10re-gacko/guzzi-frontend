1ce15f02348097f385ac4c00093a73e560174f7f
keunwoo-kang <keunwoo.kang@toss.im>
2023-06-23 16:23:47 +0900

    feat: confirm

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,15 +4,35 @@ import Button from 'components/Button';
 import Heading from 'components/Heading';
 import Txt from 'components/Txt';
 import useToaster from 'hooks/useToaster';
+import useConfirm from 'hooks/useConfirm';
 
 export default function Home() {
-  const { successToast } = useToaster();
+  const { successToast, errorToast } = useToaster();
+  const { openConfirm } = useConfirm();
+
   return (
     <>
       <Heading>거지방</Heading>
       <BottomSheet>
         <Txt size="big">안녕</Txt>
         <Spacing size={24} />
+        <Button
+          onClick={async () => {
+            const isConfirmed = await openConfirm({
+              title: '진짜 인정합니까?',
+              description: '인정하시는게 좋을걸요',
+            });
+
+            if (isConfirmed) {
+              successToast('인정하셨습니다');
+            } else {
+              errorToast('인정하지 않으셨습니다');
+            }
+          }}
+        >
+          인정합니까?
+        </Button>
+        <Spacing size={12} />
         <Button onClick={() => successToast('잘가요~')}>잘가</Button>
       </BottomSheet>
     </>
